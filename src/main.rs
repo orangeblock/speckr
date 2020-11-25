657e669d2777b115db593e9185325f79f43d3ed5
orangeblock <3149831+orangeblock@users.noreply.github.com>
2020-11-25 23:13:03 +0000

    move key expansion into struct/impl to allow reusing keys

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,46 @@
 use std::convert::TryInto;
-use std::mem::size_of;
+
+struct Key{
+    round_keys: [u16; 22]
+}
+
+/// Performs key scheduling and stores the round keys in the struct.
+///
+/// The key is split into an array of words where the 0-th 
+/// index is the first word in memory (rightmost word below). 
+/// For illustration purposes the following mental image is assumed:
+///
+/// [ <L_(i+m-2)>   ...    <L_i>   <k_i> ]
+///      idx=3     idx=2   idx=1   idx=0
+///
+/// ...which for first round (i=0) and a 4-word key (m=4) becomes:
+///
+/// [ <L_2>  <L_1>  <L_0>  <k_0> ]
+///   idx=3  idx=2  idx=1  idx=0
+///
+/// See also [sec. 4.2]: https://eprint.iacr.org/2013/404.pdf
+impl Key {
+    pub fn new(key: u64) -> Key {
+        let mut parts: [u16; 4] = bytes2words(&key.to_le_bytes())
+            .as_slice().try_into().expect("Invalid key size");
+        let mut ret = Key{ round_keys: [parts[0]; 22] };
+        for i in 0..(ret.round_keys.len()-1){
+            // generate next round keys
+            let (l, r) = speck_round_enc(
+                parts[1], parts[0], i as u16
+            );
+            // update key parts for next round
+            parts[0] = r;
+            for j in 1..parts.len()-1{
+                parts[j] = parts[j+1];
+            }
+            parts[parts.len()-1] = l;
+            // set current round key
+            ret.round_keys[i+1] = parts[0];
+        }
+        ret
+    }
+}
 
 // TODO: compare with built-in rotate_ fns.
 fn ror(x: u16, n: u8) -> u16 { x >> n | x << (16 - n) }
@@ -33,22 +74,6 @@ fn speck_round_dec(x: u16, y: u16, k: u16) -> (u16, u16) {
     (x, y)
 }
 
-/// Performs key scheduling and outputs the round keys.
-///
-/// Expects the key parts as an array of words where the 0-th 
-/// index is the first word in memory (rightmost word below). 
-/// For illustration purposes the comments in this function
-/// assume the following mental image:
-///
-/// [ <L_(i+m-2)>   ...    <L_i>   <k_i> ]
-///      idx=3     idx=2   idx=1   idx=0
-///
-/// ...which for first round (i=0) and a 4-word key (m=4) becomes:
-///
-/// [ <L_2>  <L_1>  <L_0>  <k_0> ]
-///   idx=3  idx=2  idx=1  idx=0
-///
-/// See also [sec. 4.2]: https://eprint.iacr.org/2013/404.pdf
 fn expand_key(mut parts: [u16; 4]) -> [u16; 22]{
     let mut round_keys = [parts[0]; 22];
     for i in 0..(round_keys.len()-1){
@@ -93,6 +118,18 @@ fn encrypt(block: u32, k: u64) -> u32{
     ((bl as u32) << 16) | br as u32
 }
 
+fn encrypt32(block: u32, k: &Key) -> u32 {
+    let block_vec: Vec<u16> = bytes2words(&block.to_le_bytes());
+    let mut bl = block_vec[1];
+    let mut br = block_vec[0];
+    for i in 0..22{
+        let (l, r) = speck_round_enc(bl, br, k.round_keys[i]);
+        bl = l;
+        br = r;
+    }
+    ((bl as u32) << 16) | br as u32
+}
+
 fn decrypt(block: u32, k: u64) -> u32{
     let key_vec: Vec<u16> = bytes2words(&k.to_le_bytes());
     let block_vec: Vec<u16> = bytes2words(&block.to_le_bytes());
@@ -109,11 +146,30 @@ fn decrypt(block: u32, k: u64) -> u32{
     ((bl as u32) << 16) | br as u32
 }
 
+
+fn decrypt32(block: u32, k: &Key) -> u32{
+    let block_vec: Vec<u16> = bytes2words(&block.to_le_bytes());
+    let mut bl = block_vec[1];
+    let mut br = block_vec[0];
+    for i in (0..22).rev(){
+        let (l, r) = speck_round_dec(bl, br, k.round_keys[i]);
+        bl = l;
+        br = r;
+    }
+    ((bl as u32) << 16) | br as u32
+}
+
+
 fn main() {
     let pt = 0x6574694cu32;
     let k = 0x1918111009080100u64;
-    let ct = encrypt(pt, k);
+    let key = Key::new(k);
+    let ct = encrypt32(pt, &key);
     println!("ct: {:08x}", ct);
-    let pt2 = decrypt(ct, k);
+    let pt2 = decrypt32(ct, &key);
     println!("pt: {:08x}", pt2);
+    // let f = File::open("test.txt").unwrap();
+    // let mut reader = BufReader::new(f);
+    // let mut buf = vec![0u8; 1024];
+    // reader.read_exact(&mut buf).unwrap();
 }
